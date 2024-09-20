0201005a9572017327c0e84af1504fffd1d54445
Aleksey <black-colorss@mail.ru>
2024-09-20 19:56:12 +0300

    доработка

--- src/main/java/com/app/vacationpaycalculator/contollers/CalculateController.java
+++ src/main/java/com/app/vacationpaycalculator/contollers/CalculateController.java
@@ -3,17 +3,18 @@ package com.app.vacationpaycalculator.contollers;
 import com.app.vacationpaycalculator.services.CalculateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class CalculateController {
 
-    CalculateService calculateService;
+    final private CalculateService calculateService;
 
 
-    @GetMapping("/calculate")
-    public double calculateVacationPay(double salary, int vacationsDays){
+    @GetMapping("/calculate/{salary}&{vacationsDays}")
+    public double calculateVacationPay(@PathVariable double salary, @PathVariable int vacationsDays){
         return calculateService.calculateVacationPay(salary, vacationsDays);
 
     }
