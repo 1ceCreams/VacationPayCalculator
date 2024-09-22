424d38c5dfe528b7ae36a7450bd5455f700919eb
Aleksey <black-colorss@mail.ru>
2024-09-22 21:55:53 +0300

    изменены параметры контроллера(@PathVariable -> @RequestParam)

--- src/main/java/com/app/vacationpaycalculator/contollers/CalculateController.java
+++ src/main/java/com/app/vacationpaycalculator/contollers/CalculateController.java
@@ -4,7 +4,10 @@ import com.app.vacationpaycalculator.services.CalculateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,10 +15,10 @@ public class CalculateController {
 
     private final CalculateService calculateService;
 
-
-    @GetMapping("/calculate/{salary}&{vacationsDays}")
-    public double calculateVacationPay(@PathVariable double salary, @PathVariable int vacationsDays){
-        return calculateService.calculateVacationPay(salary, vacationsDays);
+    
+    @GetMapping("/calculate")
+    public double calculateVacationPay(@RequestParam double salary,@RequestParam LocalDate startDate, @RequestParam LocalDate endDate){
+        return calculateService.calculateVacationPay(salary, startDate,endDate);
 
     }
 }
